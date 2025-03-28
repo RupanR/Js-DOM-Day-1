7861daa39a31273176771c44fb257b71d436c6eb
RupanR <rupanmentor@gmail.com>
2025-03-28 20:18:09 +0530

    session task completed

--- script.js
+++ script.js
@@ -42,4 +42,41 @@ division.append(heading,breakTag,paragraph)
 document.body.appendChild(division)
 
 */
-//! session Task
+/*
+! session Task
+const heading = document.createElement("h1")
+const heading1 = document.createElement("h1")
+const header = document.createElement("header")
+const navigation = document.createElement("nav")
+const order = document.createElement("ul")
+const list1 = document.createElement("li")
+const list2 = document.createElement("li")
+const list3 = document.createElement("li")
+const main = document.createElement("main")
+const section = document.createElement("section")
+
+heading.innerHTML = "<i>Create Element Session Task </i>"
+heading1.innerHTML ="<i>Above all are created Dynamically"
+list1.innerText= "About"
+list2.innerText= "Pricing"
+list3.innerText= "Contact"
+
+heading.classList.add("text-center", "text-3xl","text-red-700","head")
+heading1.classList.add("text-center", "text-3xl","text-red-700","head")
+header.setAttribute("class","headers")
+order.classList.add("flex","justify-center","space-evenly","gap-8")
+list1.classList.add("list-none","text-black","cursor-pointer")
+list2.classList.add("list-none","text-black","cursor-pointer")
+list3.classList.add("list-none","text-black","cursor-pointer")
+
+order.append(list1,list2,list3)
+navigation.appendChild(order)
+header.appendChild(navigation)
+section.appendChild(heading1)
+main.appendChild(section)
+document.body.append(heading,header,main)
+
+*/
+
+
+
